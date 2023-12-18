7f555706098ed0d908d247ea0f19adf81bc119ae
Mthabs <>
2023-12-18 12:21:34 +0000

    Introduce the 'UserProfile' model class for user profile creation.

--- profiles/models.py
+++ profiles/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class UserProfile(models.Model):
+    owner = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    content = models.TextField(blank=True)
+    profile_picture = models.ImageField(upload_to='image/', null=True, blank=True)
+    cover_photo = models.ImageField(upload_to='image/', null=True, blank=True)
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return f"{self.owner}'s profile"

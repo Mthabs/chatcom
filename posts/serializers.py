10c4c4a9f39416ded48a213e209ea50f30d72474
Mthabs <>
2023-12-18 14:59:21 +0000

    Develop 'Posts' serializers class and added representation instance for default images

--- posts/serializers.py
+++ posts/serializers.py
@@ -12,4 +12,15 @@ class PostSerializer(serializers.ModelSerializer):
         model = Post
         fields = ['id', 'profile_id', 'owner', 'header', 'content', 'created_at', 'updated_at','profile_picture', 'post_picture', 'is_owner', 'image_filter']
 
-        
+    def to_representation(self, instance):
+        representation = super().to_representation(instance)
+        default_post_picture_url = 'https://res.cloudinary.com/dnt7oro5y/image/upload/v1702078965/default_post_wv7bvz.jpg'
+        default_profile_picture_url = 'https://res.cloudinary.com/dnt7oro5y/image/upload/v1702078965/default_profile_yansvo.jpg'
+
+        if not instance.post_picture:
+            representation['post_picture'] = default_post_picture_url
+
+        if 'profile_picture' not in representation:
+            representation['profile_picture'] = default_profile_picture_url
+
+        return representation    

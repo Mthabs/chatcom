64fc88bb2c00ed36f69bf708bc0732b6f515937a
Mthabs <>
2023-12-18 15:49:21 +0000

    Implement 'followers' list views, providing users with a comprehensive list of all followers.

--- followers/views.py
+++ followers/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from friends_chats.permissions import IsOwnerOrReadOnly
+from .models import Follower
+from .serializers import FollowerSerializer
 
-# Create your views here.
+
+class FollowerListCreateView(generics.ListCreateAPIView):
+    permission_classes = [permissions.IsAuthenticated]
+    queryset = Follower.objects.all()
+    serializer_class = FollowerSerializer

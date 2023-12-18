6390d51719ac4674da08b7388e21372e71719aa5
Mthabs <>
2023-12-18 20:11:31 +0000

    Introduce 'Comments' list views to display a comprehensive list of all comments.

--- comments/views.py
+++ comments/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from friends_chats.permissions import IsOwnerOrReadOnly
+from .models import Comment
+from .serializers import CommentSerializer
 
-# Create your views here.
+class CommentListCreateView(generics.ListCreateAPIView):
+    permission_classes = [permissions.IsAuthenticated, IsOwnerOrReadOnly]
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+    

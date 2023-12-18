7aad4334ff5c3aa202dfc9fc4844373ed436896c
Mthabs <>
2023-12-18 17:35:50 +0000

    Introduce 'Photos' serializers class to validate image serialization tasks

--- photos/serializers.py
+++ photos/serializers.py
@@ -10,3 +10,15 @@ class PhotoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Photo
         fields = ['id', 'owner', 'image', 'caption', 'created_at', 'updated_at', 'is_owner']
+
+
+    def validate_image(self, value):
+        # Check file size
+        max_size = 3 * 1024 * 1024  # Maximum size allowed (2MB)
+
+        if value.size > max_size:
+            raise serializers.ValidationError(f"File size too large. Max size is {max_size} bytes.")
+
+        return value
+
+    

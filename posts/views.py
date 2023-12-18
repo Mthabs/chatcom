ae398e28f91ae86233fa01077099743b17f5f762
Mthabs <>
2023-12-18 15:13:03 +0000

    Introduce 'Posts' list views enabling users to access a comprehensive list of all posts.

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions, filters
+from friends_chats.permissions import IsOwnerOrReadOnly
+from .models import Post
+from .serializers import PostSerializer
 
-# Create your views here.
+class PostListView(generics.ListCreateAPIView):
+    permission_classes = [permissions.IsAuthenticated, IsOwnerOrReadOnly]
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer

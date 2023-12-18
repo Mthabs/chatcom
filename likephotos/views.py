6dfda54b6d06cb667b295b8b3a84f5e8a5194929
Mthabs <>
2023-12-18 18:56:24 +0000

    Introduce 'LikePhotos' list views to display a comprehensive list of likes on photos.

--- likephotos/views.py
+++ likephotos/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from friends_chats.permissions import IsOwnerOrReadOnly
+from .models import Likephoto
+from .serializers import LikephotoSerializer
 
-# Create your views here.
+class LikephotoCreateView(generics.ListCreateAPIView):
+    permission_classes = [permissions.IsAuthenticated, IsOwnerOrReadOnly]
+    queryset = Likephoto.objects.all()
+    serializer_class = LikephotoSerializer
+    

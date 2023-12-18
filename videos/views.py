305c5115e54666990d868c11bc6bf9d9df37c3ad
Mthabs <>
2023-12-18 18:12:10 +0000

    add perform create to allow users to create videos

--- videos/views.py
+++ videos/views.py
@@ -8,3 +8,6 @@ class VideoListCreateView(generics.ListCreateAPIView):
     permission_classes = [permissions.IsAuthenticated, IsOwnerOrReadOnly]
     queryset = Video.objects.all()
     serializer_class = VideoSerializer
+
+def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)

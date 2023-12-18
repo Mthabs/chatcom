b16de09c787c38684097f4905e5d9b3269ceb39f
Mthabs <>
2023-12-18 13:50:54 +0000

    add profiles perfom create method to allow authenticated users to create profiles

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -3,7 +3,22 @@ from .models import UserProfile
 
 class UserProfileSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
-    
+
     class Meta:
         model = UserProfile
         fields = ['id', 'owner', 'created_at', 'updated_at', 'name', 'content', 'profile_picture', 'cover_photo']
+
+    def to_representation(self, instance):
+        if isinstance(instance, UserProfile):
+            representation = super().to_representation(instance)
+            default_profile_picture_url = 'https://res.cloudinary.com/dnt7oro5y/image/upload/v1702078965/default_profile_yansvo.jpg'
+            default_cover_photo_url = 'https://res.cloudinary.com/dnt7oro5y/image/upload/v1702078965/default_profile_ifketo.jpg'
+                
+            if not instance.profile_picture:
+                representation['profile_picture'] = default_profile_picture_url
+
+            if not instance.cover_photo:
+                representation['cover_photo'] = default_cover_photo_url
+
+            return representation
+        return {}
--- profiles/views.py
+++ profiles/views.py
@@ -8,7 +8,8 @@ class UserProfileListCreateView(generics.ListCreateAPIView):
     queryset = UserProfile.objects.all()
     serializer_class = UserProfileSerializer
 
-    
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
 
 
 class UserProfileDetailView(generics.RetrieveUpdateDestroyAPIView):

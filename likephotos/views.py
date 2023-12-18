a9e4ffd086fdc1328dcc6af8cda51a46acda622f
Mthabs <>
2023-12-18 18:57:14 +0000

    add perform create to allow users to create likephotos

--- likephotos/views.py
+++ likephotos/views.py
@@ -7,4 +7,7 @@ class LikephotoCreateView(generics.ListCreateAPIView):
     permission_classes = [permissions.IsAuthenticated, IsOwnerOrReadOnly]
     queryset = Likephoto.objects.all()
     serializer_class = LikephotoSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
     

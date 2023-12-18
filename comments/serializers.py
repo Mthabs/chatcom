0113730d9b206af61bdb1fc10026c44ce7dd549a
Mthabs <>
2023-12-18 20:10:17 +0000

    Create 'Comments' serializers class for streamlined data serialization.

---
+++ comments/serializers.py
@@ -0,0 +1,22 @@
+from rest_framework import serializers
+from .models import Comment
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
+    profile_id = serializers.ReadOnlyField(source='owner.userprofile.id')
+    profile_picture = serializers.ReadOnlyField(source='owner.userprofile.profile_picture_url')
+    is_owner = serializers.SerializerMethodField()
+    created_at = serializers.SerializerMethodField()
+    updated_at = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Comment
+        fields = ['id', 'profile_id', 'owner', 'post', 'content', 'created_at', 'updated_at', 'profile_picture', 'is_owner']
+
+    def get_is_owner(self, obj):
+        request = self.context.get('request')
+        return request.user == obj.owner
+    
+    class CommentDetailSerializer(CommentSerializer):
+    post = serializers.ReadOnlyField(source='post.id')

925478c35d9275de88a7f970ed3808adcee76187
Mthabs <>
2023-12-19 00:05:10 +0000

    add herokuapp url to allowed hosts

--- friends_chats/settings.py
+++ friends_chats/settings.py
@@ -60,7 +60,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEV' in os.environ
 
-ALLOWED_HOSTS = ['8000-mthabs-chatcom-0fykrn98fh.us2.codeanyapp.com']
+ALLOWED_HOSTS = ['8000-mthabs-chatcom-0fykrn98fh.us2.codeanyapp.com','chatcom-ec4ad238849d.herokuapp.com']
 
 
 # Application definition

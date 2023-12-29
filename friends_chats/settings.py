d5f4cbd020b833b346484763e477d9f1d051e9ab
Mthabs <>
2023-12-29 19:44:02 +0000

    add changes to the settings

--- friends_chats/settings.py
+++ friends_chats/settings.py
@@ -48,7 +48,7 @@ JWT_AUTH_REFRESH_COOKIE = 'my-refresh-token'
 JWT_AUTH_SAMESITE = 'None'
 
 REST_AUTH_SERIALIZERS = {
-    'USER_DETAILS_SERIALIZER': 'drf_api.serializers.CurrentUserSerializer'
+    'USER_DETAILS_SERIALIZER': 'friends_chats.serializers.CurrentUserSerializer'
 }
 
 # Quick-start development settings - unsuitable for production
